1fa1cdbb6671918cf05114ab247b6ad9599a6bc7
MMunim <mmunim9.9.01@gmail.com>
2025-10-05 21:51:39 +0600

    explore long long integer and double

---
+++ Module 1/big_data_type.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    // int a = 10000000000;
+    // long long int a = 100000000000000;
+    // printf("%lld", a);
+
+    // float f = 2134.56789543;
+    double f = 213444.56789543;
+    printf("%lf", f);
+
+    return 0; 
+}
