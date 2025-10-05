34d7ca0527096ac05d9d5d594d160238dff43d3b
MMunim <mmunim9.9.01@gmail.com>
2025-10-06 02:45:48 +0600

    explore arithmetic in c

---
+++ Module 2/arithmetic.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+    int a = 3;
+    float b = 7;
+
+    // int sum = a + b;
+    // printf("Summation = %d\n", sum);
+
+    // int sub = b - a;
+    // printf("Subtraction = %d\n", sub); 
+
+    // int mul = a * b;
+    // printf("Multiplication = %d\n", mul);
+
+    float div = b / a;
+    printf("Division = %f\n", div);
+
+    return 0;
+}
