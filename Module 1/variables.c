433b1af847f6cc217227651e522b87cc794032a1
MMunim <mmunim9.9.01@gmail.com>
2025-10-05 20:14:28 +0600

    variables

---
+++ Module 1/variables.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int num1 = 10;
+    num1 = 33;
+    float f = 4.56;
+    char c = '9';
+    printf("%d\n", num1);
+    printf("%.3f\n", f);
+    printf("%c\n", c);
+    return 0;
+}
