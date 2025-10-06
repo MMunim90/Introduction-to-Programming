c3fb3946e8bcbe107c6cc34d9aa32d93cdd9156b
MMunim <mmunim9.9.01@gmail.com>
2025-10-06 19:43:58 +0600

    soveld practice problem 2

--- Module 2/practice2.c
+++ Module 2/practice2.c
@@ -2,5 +2,17 @@
 int main()
 {
     int a;
-     scanf("%d", &a);
+    scanf("%d", &a);
+
+    if(a > 0){
+        printf("This Number is Positive");
+    }
+    else if(a == 0){
+        printf("This Number is Zero");
+    }
+    else{
+        printf("This Number is Negative");
+    }
+
+    return 0;
 }
