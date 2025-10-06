07bd5f9c1253d085f739592fc125d8194443125d
MMunim <mmunim9.9.01@gmail.com>
2025-10-06 18:49:56 +0600

    another version of nested if else

--- Module 2/nested_if_else.c
+++ Module 2/nested_if_else.c
@@ -3,21 +3,34 @@ int main()
 {
     int tk;
     scanf("%d", &tk);
-    if(tk >= 5000)
-    {
-        printf("Going to cox\n");
-        if(tk >= 10000)
-        {
-            printf("Going to saint Martin\n");
-        }
-        else
-        {
-            printf("Go back to home\n");
-        }
+    // if(tk >= 5000)
+    // {
+    //     printf("Going to cox\n");
+    //     if(tk >= 10000)
+    //     {
+    //         printf("Going to saint Martin\n");
+    //     }
+    //     else
+    //     {
+    //         printf("Go back to home\n");
+    //     }
+    // }
+    // else
+    // {
+    //     printf("Don't go to anywhere\n");
+    // }
+
+    if(tk >= 5000){
+        printf("going to cox bazar\n");
     }
-    else
-    {
-        printf("Don't go to anywhere\n");
+    if(tk >= 6000){
+        printf("going to sajek\n");
+    }
+    if(tk >= 10000){
+        printf("going to sundarban\n");
+    }
+    else{
+        printf("hello\n");
     }
 
     return 0;
