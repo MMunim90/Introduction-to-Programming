2d49e5c70ddfc75ad41601fd2b6ae77fa3f06982
MMunim <mmunim9.9.01@gmail.com>
2025-10-07 00:46:56 +0600

    done all practice day problem

---
+++ Practice day 1/problem1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+
+    scanf("%d", &a);
+    if(a == 0)
+    {
+        printf("Zero\n");
+    }
+    else
+    {
+        printf("Non Zero\n");
+    }
+
+    return 0;
+}
