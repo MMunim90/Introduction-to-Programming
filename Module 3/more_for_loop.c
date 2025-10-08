1b96324ca623680a05b482b47b3be227f1d9270e
MMunim <mmunim9.9.01@gmail.com>
2025-10-08 18:59:03 +0600

    explore some cases in for loop

---
+++ Module 3/more_for_loop.c
@@ -0,0 +1,50 @@
+// print all even and odd number between 0 to 100 by for loop
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=0; i<=100; i=i+2)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+// print all the multiple of 4 between 4 to 100 by for loop
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=4; i<=100; i=i+4)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+
+// print all number between 100 to 0 reversely by loop
+
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=100; i>=1; i=i-1)
+//     {
+//         printf("%d\n", i);
+//     }
+
+//     return 0;
+// }
+
+//print power of 2 by for loop
+#include<stdio.h>
+int main()
+{
+    for(int i=2; i<=1024; i=i*2)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
