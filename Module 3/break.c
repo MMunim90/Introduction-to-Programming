5719884ae581bbb02905151740e87d233afa674a
MMunim <mmunim9.9.01@gmail.com>
2025-10-08 23:49:56 +0600

    break statement

---
+++ Module 3/break.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=1; i<=10; i++)
+    {
+        if (i == 9)
+        {
+            break;
+        }
+        
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
