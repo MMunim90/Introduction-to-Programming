c7b9b9ea8b9cf690da40e711ae200f150363a403
MMunim <mmunim9.9.01@gmail.com>
2025-10-12 19:33:00 +0600

    first digit problem solved

--- Module 5/first_digit.c
+++ Module 5/first_digit.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    
+    int n, div;
+    scanf("%d", &n);
+    div = n/1000;
+
+    if(div%2 == 0)
+    {
+        printf("EVEN");
+    }
+    else
+    {
+        printf("ODD");
+    }
     return 0;
 }
