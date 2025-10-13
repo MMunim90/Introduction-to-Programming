73e7ebd9b65859b30196ef819a267f5ccc0b85d6
MMunim <mmunim9.9.01@gmail.com>
2025-10-13 15:13:10 +0600

    solve even numbers

---
+++ Module 6/Even_number.c
@@ -0,0 +1,24 @@
+// this is wrong code
+
+// #include <stdio.h>
+// int main()
+// {
+//     int n, i;
+//     scanf("%d", &n);
+
+//     if (i >= 2)
+//     {
+//         for (i = 1; i <= n; i++)
+//         {
+//             if (i % 2 == 0)
+//             {
+//                 printf("%d\n", i);
+//             }
+//         }
+//     }
+//     else
+//     {
+//         printf("-1");
+//     }
+//     return 0;
+// }
