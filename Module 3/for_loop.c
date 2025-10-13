fa3517fe6d6f27bf1120d1d630837e111a9f75a9
MMunim <mmunim9.9.01@gmail.com>
2025-10-13 07:17:58 +0600

    for loop

--- Module 3/for_loop.c
+++ Module 3/for_loop.c
@@ -1,10 +1,49 @@
-#include<stdio.h>
-int main()
-{
-   for(int i=1; i<=9; i=i+1)
-   {
-     printf("hello\n");
-   }
+// #include<stdio.h>
+// int main()
+// {
+//    for(int i=1; i<=9; i=i+1)
+//    {
+//      printf("hello\n");
+//    }
 
-    return 0;
-}
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//    for(int i=5; i<=10; i++)
+//    {
+//      printf("hello\n");
+//      i++;
+//    }
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//   int i = 0, j = 0;
+//    for(j=1; j<=5; j++)
+//    {
+//      i++;
+//    }
+//    printf("%d", j);
+
+//     return 0;
+// }
+
+
+//error
+// #include<stdio.h>
+// int main()
+// {
+//    for(int i=5; i<=10; i++)
+//    {
+//      int x = 4;
+//    }
+//    printf("%d", i);
+
+//     return 0;
+// }
