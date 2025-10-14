703448f4fa69e3036055b9add9afc36dd92152fd
MMunim <mmunim9.9.01@gmail.com>
2025-10-14 19:35:05 +0600

    explore nested loop

---
+++ Module 6/nested_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    for (int j = 1; j <= 9; j++)
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            // printf("%d ", i);
+            printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
