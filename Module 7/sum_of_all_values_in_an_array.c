80603c4ee9b3109b513b0c5ee899305ffb4cf27c
MMunim <mmunim9.9.01@gmail.com>
2025-10-16 23:46:47 +0600

    print all even number in an array

---
+++ Module 7/sum_of_all_values_in_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n, sum=0;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+        printf("%d ", arr[i]);
+    }
+    printf("\n%d", sum);
+    
+    return 0;
+}
