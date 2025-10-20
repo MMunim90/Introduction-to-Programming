a8c4d65974fb09fd937bd9e6147d2e21ba61e481
MMunim <mmunim9.9.01@gmail.com>
2025-10-20 22:22:55 +0600

    calculate length of a string

---
+++ Module 10/length_of_string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char string[101];
+    scanf("%s", &string);
+    // printf("%s", string);
+    
+    int count = 0;
+    for(int i=0; string[i] != '\0'; i++)
+    {
+        count++;
+    }
+
+    printf("%d", count);
+    return 0;
+}
