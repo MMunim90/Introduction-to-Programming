102c48b3fd1265f94f53ad849fce26cdbe87452b
MMunim <mmunim9.9.01@gmail.com>
2025-10-24 14:42:26 +0600

    explore strcat function

---
+++ Module 11/string_concat_func.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[101], b[101];
+    scanf("%s %s", &a, &b);
+
+    strcat(b,a); //this function copy the second string and stote it to the first.
+
+    printf("%s %s", a, b);
+    
+    return 0;
+}
