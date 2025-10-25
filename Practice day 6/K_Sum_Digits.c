5b36e807eca79d49560c6e3f7f4ae41e9b6c34b1
MMunim <mmunim9.9.01@gmail.com>
2025-10-26 02:38:31 +0600

    sum digit problem solved

---
+++ Practice day 6/K_Sum_Digits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    char str[n];
+
+    scanf("%s", str);
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        count += str[i] - '0';
+    }
+
+    printf("%d", count);
+
+    return 0;
+}
