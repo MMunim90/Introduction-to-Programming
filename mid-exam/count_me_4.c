03e95b10dac4c159e03ffc7ead1a30768f32b06c
MMunim <mmunim9.9.01@gmail.com>
2025-10-26 20:41:25 +0600

    count me 4 problem solved

---
+++ mid-exam/count_me_4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char a[10001];
+    scanf("%s", a);
+
+    int length = strlen(a);
+
+    int fre[27] = {0};
+
+    for (int i = 0; i < length; i++)
+    {
+        int idx = a[i] - 'a';
+        fre[idx]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (fre[i] != 0)
+        {
+            printf("%c - %d\n", i + 'a', fre[i]);
+        }
+    }
+
+    return 0;
+}
