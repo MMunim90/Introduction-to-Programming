4430ace3fb8db0102ab9cc569d7ef8aa96eeff81
MMunim <mmunim9.9.01@gmail.com>
2025-10-27 17:13:59 +0600

    replace min max problem solved

--- Module 9/M_Replace_MinMax.c
+++ Module 9/M_Replace_MinMax.c
@@ -1,44 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n;
     scanf("%d", &n);
     int arr[n];
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
     int min = arr[0];
     int max = arr[0];
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        if(min > arr[i])
+        if (min > arr[i])
         {
             min = arr[i];
         }
-        else if(max <= arr[i])
+        else if (max <= arr[i])
         {
             max = arr[i];
         }
     }
 
-    printf("%d %d", min, max);
-    printf("\n");
-    
     int temp = min;
     min = max;
     max = temp;
-    
-    printf("%d %d", min, max);
-    printf("\n");
-    for(int i=0; i<n; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i] == max)
+        if (arr[i] == max)
         {
             arr[i] = min;
         }
-        if(arr[i] == min)
+        else if (arr[i] == min)
         {
             arr[i] = max;
         }
