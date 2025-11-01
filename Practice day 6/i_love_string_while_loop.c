9e8516914773470b0d4e84b9476c457493b7fed9
MMunim <mmunim9.9.01@gmail.com>
2025-11-01 10:59:04 +0600

    I love string problem solved using while loop

---
+++ Practice day 6/i_love_string_while_loop.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    while (n--)
+    {
+        char a[55], b[55];
+
+        scanf("%s %s", a, b);
+
+        int a_len = strlen(a);
+        int b_len = strlen(b);
+
+        int min_len;
+
+        if (a_len > b_len)
+        {
+            min_len = b_len;
+        }
+        else
+        {
+            min_len = a_len;
+        }
+
+        // for common part
+        int i=0, j=0;
+        while( i < a_len && j < b_len )
+        {
+            printf("%c%c", a[i], b[i]);
+            i++;
+            j++;
+        }
+
+        // if a string is big
+        while( i < a_len )
+        {
+            printf("%c", a[i]);
+            i++;
+        }
+
+        // if b string is big
+        while( j < b_len )
+        {
+            printf("%c", b[i]);
+            j++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ Practice day 6/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+ // while( i < a_len && j < b_len )
+        // {
+        //     printf("%c%c", a[i], b[i]);
+        //     i++;
+        //     j++;
+        // }
