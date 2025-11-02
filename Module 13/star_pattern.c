78d9462ae8b493d50283c27513a6534aec454506
MMunim <mmunim9.9.01@gmail.com>
2025-11-02 11:44:08 +0600

    printing star in a different way

--- Module 13/star_pattern.c
+++ Module 13/star_pattern.c
@@ -1,16 +1,37 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     for(int i=1; i<=n; i++) // for printing line
+//     {
+//         for(int j=i; j>=1; j--)  // for printing star
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+// another way to printing star
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n, star=1;
+
     scanf("%d", &n);
 
-    for(int i=1; i<=n; i++) // for printing line
+    for(int i=0; i<n; i++)
     {
-        for(int j=i; j>=1; j--)  // for printing star
+        for(int j=1; j<=star; j++)
         {
             printf("*");
         }
         printf("\n");
+        star++;
     }
     return 0;
 }
