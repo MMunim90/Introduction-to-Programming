9ee47eb140d6559f9bdb526c0f0374b2f0846a33
MMunim <mmunim9.9.01@gmail.com>
2025-11-03 12:29:07 +0600

    practice 2 done

--- Module 13/practice_2.c
+++ Module 13/practice_2.c
@@ -1,25 +1,25 @@
-// #include<stdio.h>
-// int main()
-// {
-//     int n;
-//     scanf("%d", &n);
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
 
-//     int num = 1;
-//     int space = n-1;
+    int num = 1;
+    int space = n-1;
 
-//     for(int i=0; i<n; i++)
-//     {
-//         for(int j=1; j<=space; j++)
-//         {
-//             printf(" ");
-//         }
-//         for(int j=1; j<=num; j++)
-//         {
-//             printf("%d ", num);
-//             num++;
-//         }
-//         printf("\n");
-//         space--;
-//     }
-//     return 0;
-// }
+    for(int i=0; i<n; i++)
+    {
+        for(int j=1; j<=space; j++)
+        {
+            printf(" ");
+        }
+        for(int j=1; j<=num; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+        num++;
+        space--;
+    }
+    return 0;
+}
---
+++ Module 13/practice_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int star = (n * 2) - 1;
+    int space = 0;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; )
+    }
+    return 0;
+}
