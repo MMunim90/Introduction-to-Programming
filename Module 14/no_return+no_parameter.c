4ae1857a1510a194a6a5e1c485e951194fc9527a
MMunim <mmunim9.9.01@gmail.com>
2025-11-04 13:51:11 +0600

    function no return + no parameter

---
+++ Module 14/no_return+no_parameter.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void sum()
+{
+    int a,b;
+    scanf("%d %d", &a, &b);
+    int res = a + b;
+    printf("%d", res);
+}
+
+int main()
+{
+    sum();
+    // printf("Hello");
+    return 0;
+}
