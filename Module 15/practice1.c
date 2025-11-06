2711f6671c5cd5e011e763c35747f7f355211595
MMunim <mmunim9.9.01@gmail.com>
2025-11-06 06:36:02 +0600

    practice problem 1 done

---
+++ Module 15/practice1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int swaping(int* x, int* y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main()
+{
+    int x,y;
+    scanf("%d %d", &x, &y);
+
+    swaping(&x, &y);
+
+    printf("%d %d", x, y);
+    return 0;
+}
