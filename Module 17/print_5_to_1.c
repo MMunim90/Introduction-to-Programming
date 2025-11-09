65f740371b8d7caeda40ac81fab1534f42fa2e3f
MMunim <mmunim9.9.01@gmail.com>
2025-11-09 18:27:27 +0600

    simpler way to print 5 to 1 using recursion

--- Module 17/print_5_to_1.c
+++ Module 17/print_5_to_1.c
@@ -1,18 +1,40 @@
+// #include<stdio.h>
+
+// void hello(int i)
+// {
+//     if(i == 0)
+//     {
+//         return;
+//     }
+
+//     printf("%d\n", i);
+//     hello(i-1);
+// }
+
+// int main()
+// {
+//     hello(5);
+//     return 0;
+// }
+
+
+// another recursive way
+
 #include<stdio.h>
 
 void hello(int i)
 {
-    if(i == 0)
+    if(i == 6)
     {
         return;
     }
 
+    hello(i+1);
     printf("%d\n", i);
-    hello(i-1);
 }
 
 int main()
 {
-    hello(5);
+    hello(1);
     return 0;
 }
