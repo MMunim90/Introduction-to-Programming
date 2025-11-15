e515be6b4e9c039135c89076ef9e0b02691ede4b
MMunim <mmunim9.9.01@gmail.com>
2025-11-16 01:16:27 +0600

    sort number in a sort way

---
+++ Module 19/sort_number_short_way.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int max=a, mid=a, min=a;
+    if(b>max)
+    {
+        max = b;
+    } 
+    if(c>max)
+    {
+        max = c;
+    }
+
+    if(b<min)
+    {
+        min = b;
+    }
+    if(c<min)
+    {
+        min = c;
+    }
+
+    mid = (a+b+c) - (max+min);
+
+    printf("%d\n%d\n%d\n\n%d\n%d\n%d", min, mid, max, a, b, c);
+    return 0;
+}
