ef98cccdab1bb3d6856c2b73747fdc1c3b02fbf6
MMunim <mmunim9.9.01@gmail.com>
2025-11-16 23:31:09 +0600

    solve 2 question codeforces

---
+++ Extra problem solve in codeforces/N_Shift_Zeros.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void sft_0(int arr[], int n)
+{
+
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i] == 0)
+        {
+            int temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
+        printf("%d ", arr[i]);
+    }
+
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    sft_0(arr, n);
+    return 0;
+}
--- Extra problem solve in codeforces/Y_The_last_2_digits.c
+++ Extra problem solve in codeforces/Y_The_last_2_digits.c
@@ -1,20 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     long long int a, b, c, d;
     scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
 
-    
-    long long int multiply = a * b * c * d;
-    if(multiply > 100000000000000000)
+    if (a && b && c && d >= 2)
     {
-        printf("00");
-        return 0;
-    }
-    
-    long long int last2_digit = multiply % 100;
+        long long int multiply = a * b * c * d;
 
-    printf("%lld", last2_digit);
+        int last2_digit = multiply % 100;
+
+        printf("%d", last2_digit);
+    }
 
     return 0;
 }
