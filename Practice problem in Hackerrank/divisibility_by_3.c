fd6fba5a56ed04d6ed05d8bb234ab1f004015657
MMunim <mmunim9.9.01@gmail.com>
2025-11-16 13:16:03 +0600

    something change

--- Practice problem in Hackerrank/divisibility_by_3.c
+++ Practice problem in Hackerrank/divisibility_by_3.c
@@ -3,38 +3,38 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    printf("%d", &n);
+    // printf("%d ", n);
 
-    // int a;
-    // for(int i=0; i<n; i++)
-    // {
-    //     scanf("%d", &a);
-    // }
+    int a;
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a);
+    }
     // for(int i=0; i<n; i++)
     // {
     //     printf("%d", a);
     // }
 
-    // int count = 0;
+    int count = 0;
 
-    // for(int i=1; i<=n; i++)
-    // {
-    //     printf("%d\n", a);
-    //     int x = a % 10;
-    //     count = count + x;
-    //     a = a / 10;
-    //     printf("%d\n", n);
-    // }
+    for(int i=1; i<=n; i++)
+    {
+        // printf("%d\n", a);
+        int x = a % 10;
+        count = count + x;
+        a = a / 10;
+        // printf("%d\n", n);
+    }
 
-    // if(count % 3 == 0)
-    // {
-    //     printf("%d\n", count);
-    //     printf("YES");
-    // }
-    // else
-    // {
-    //     printf("%d\n", count);
-    //     printf("NO");
-    // }
+    if(count % 3 == 0)
+    {
+        // printf("%d\n", count);
+        printf("YES");
+    }
+    else
+    {
+        // printf("%d\n", count);
+        printf("NO");
+    }
     return 0;
 }
