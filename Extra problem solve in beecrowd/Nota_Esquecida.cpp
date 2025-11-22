95f6b0f986e2bd7ddb9590b0a9c073ce6342433d
MMunim <mmunim9.9.01@gmail.com>
2025-11-22 23:19:40 +0600

    Nota Esquecida problem solved

---
+++ Extra problem solve in beecrowd/Nota_Esquecida.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, m;
+    cin >> a >> m;
+
+    b = (2 * m) - a;
+
+    cout << b << endl;
+    return 0;
+}
---
+++ Extra problem solve in beecrowd/small_unlucky_number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin >> n;
+
+    for(int i=0; i<n-1; i++)
+    {
+        for(int j=i+1; j<n; j++)
+        {
+            cout << n;
+        }
+    }
+    return 0;
+}
