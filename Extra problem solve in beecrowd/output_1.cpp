5332aa91e344ea5b3c2b58da1d9abac3219fa10f
MMunim <mmunim9.9.01@gmail.com>
2025-12-02 23:16:19 +0600

    output 1 problem solved

---
+++ Extra problem solve in beecrowd/output_1.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=1; i<=39; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+    for(int i=1; i<=5; i++)
+    {
+        cout << "|                                     |" << endl;
+    }
+    for(int i=1; i<=39; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+    return 0;
+}
