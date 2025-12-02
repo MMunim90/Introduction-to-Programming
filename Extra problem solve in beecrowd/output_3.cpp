1ddc15e7be9dd19cca854a2ef333e035d3b61255
MMunim <mmunim9.9.01@gmail.com>
2025-12-03 01:10:59 +0600

    output 6 problem solved

---
+++ Extra problem solve in beecrowd/output_3.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 39; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+    cout << endl;
+
+    cout << "|x = 35                               |" << endl;
+    cout << endl;
+    cout << "|                                     |" << endl;
+    cout << endl;
+    cout << "|                x = 35               |" << endl;
+    cout << endl;
+    cout << "|                                     |" << endl;
+    cout << endl;
+    cout << "|                               x = 35|" << endl;
+    cout << endl;
+
+    for (int i = 1; i <= 39; i++)
+    {
+        cout << "-";
+    }
+
+    cout << endl;
+    cout << endl;
+    return 0;
+}
