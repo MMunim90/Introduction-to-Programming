e655617097e0319342f7c1139f95b56c7c54e8bf
MMunim <mmunim9.9.01@gmail.com>
2025-12-02 23:22:26 +0600

    output 2 problem solved

---
+++ Extra problem solve in beecrowd/output_2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for (int i = 1; i <= 39; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+
+    cout << "|        Roberto                      |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|        5786                         |" << endl;
+    cout << "|                                     |" << endl;
+    cout << "|        UNIFEI                       |" << endl;
+
+    for (int i = 1; i <= 39; i++)
+    {
+        cout << "-";
+    }
+    cout << endl;
+    return 0;
+}
