622c47e6dda72903d83c320b700f2e4c4f9cb684
MMunim <mmunim9.9.01@gmail.com>
2025-12-03 01:29:41 +0600

    output 7 problem solved

---
+++ Extra problem solve in beecrowd/output_7.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x = 96;
+    
+    for(int i=1; i<=26; i++)
+    {
+        char a = '`' + i;
+        cout << x+i << " e " << a << endl;
+    }
+
+    return 0;
+}
