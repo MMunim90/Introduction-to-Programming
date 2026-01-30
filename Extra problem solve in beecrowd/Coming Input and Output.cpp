14b691c33019efd569c54bf560fb634a4a1e6516
MMunim <mmunim9.9.01@gmail.com>
2026-01-30 13:23:17 +0600

    coming input and output problem solved

---
+++ Extra problem solve in beecrowd/Coming Input and Output.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin , s);
+
+    for(int i=0; i<s.size(); i++)
+    {
+        if(s[i] == ',')
+        {
+            cout << '\n';
+            continue;
+        }
+        cout << s[i];
+    }
+
+    cout << endl;
+    return 0;
+}
